88394bd333b5c0453880d2fc410578ce73a92c8a
Paula Cristina Gomes <grupofiaptechchallenge@gmail.com>
2024-11-03 18:30:56 -0300

    config inicial

---
+++ microsservico-pedidos/src/main/java/com/br/microsservico_pedidos/MicrosservicoPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.br.microsservico_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosservicoPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosservicoPedidosApplication.class, args);
+	}
+
+}
---
+++ microsservico-pedidos/src/test/java/com/br/microsservico_pedidos/MicrosservicoPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.microsservico_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosservicoPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
